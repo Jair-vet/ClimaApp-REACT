e835a1a3cbee29cc856ef7f26067fe38fa2392c2
Jair <jairaceves56@gmail.com>
2023-03-09 16:13:53 -0600

    Creamos los datos de busqueda en el Provider

--- src/context/ClimaProvider.jsx
+++ src/context/ClimaProvider.jsx
@@ -4,9 +4,24 @@ const ClimaContext = createContext()
 
 const ClimaProvider = ({ children }) => {
 
+    const [busqueda, setBusqueda] = useState({
+        ciudad: '',
+        pais: ''
+    })
+
+    const datosBusqueda = e => {
+        setBusqueda({
+            ...busqueda,
+            [e.target.name]: e.target.value
+        })
+    }
+
     return(
         <ClimaContext.Provider
-            value={{}}
+            value={{
+                busqueda, 
+                datosBusqueda
+            }}
         >
             {children}
         </ClimaContext.Provider>
