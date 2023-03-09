9e3e5650522f8c82be0fdde3cf3bcd4230ba5cc0
Jair <jairaceves56@gmail.com>
2023-03-09 16:43:19 -0600

    Extraemos la informacion de la API

--- src/context/ClimaProvider.jsx
+++ src/context/ClimaProvider.jsx
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import { useState, createContext } from 'react';
 
 const ClimaContext = createContext()
@@ -16,8 +17,20 @@ const ClimaProvider = ({ children }) => {
         })
     }
 
-    const consultarClima = datos => {
-        console.log(datos);
+    const consultarClima = async datos => {
+        try {
+            const { ciudad, pais } = datos
+            const appId = import.meta.env.VITE_API_KEY
+            const url = `http://api.openweathermap.org/geo/1.0/direct?q=${ciudad},${pais}&limit=1&appid=${appId}`
+
+            const { data } = await axios(url)
+            const { lat, lon } = data[0]
+
+            // const urlClima = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${appId}`
+
+        } catch (error) {
+            console.log(error);
+        }
     }
 
     return(
