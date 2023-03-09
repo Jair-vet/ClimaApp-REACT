6b2b9a77e5a2a608aeac1fbdc75d7b2464b0b8b0
Jair <jairaceves56@gmail.com>
2023-03-09 16:23:24 -0600

    Agregamos alerta para que ambos campos se envien

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,39 +1,65 @@
+import { useState } from "react"
+import useClima from "../hooks/useClima"
 
 export const Formulario = () => {
-  return (
-    <div className="contenedor">
-        <form>
-            <div className="campo">
-                <label htmlFor="ciudad">Ciudad</label>
+
+    const [alerta, setAlerta] = useState('')
+    const { busqueda, datosBusqueda } = useClima()
+
+    const { ciudad, pais } = busqueda
+
+    const handleSubmit = e => {
+        e.preventDefault()
+
+        if(Object.values(busqueda).includes('')){
+            setAlerta('Todos los Campos son Obligatorios')
+            return
+        }
+
+    }
+
+    return (
+        <div className="contenedor">
+
+            {alerta && <p>{alerta}</p>}
+            <form
+                onSubmit={handleSubmit}
+            >
+                <div className="campo">
+                    <label htmlFor="ciudad">Ciudad</label>
+                    <input 
+                        type="text" 
+                        id="ciudad"
+                        name="ciudad"
+                        onChange={datosBusqueda}
+                        value={ciudad}
+                        />
+                </div>
+                <div className="campo">
+                    <label htmlFor="ciudad">Pais</label>
+                    <select 
+                        name="pais" 
+                        id="pais"
+                        onChange={datosBusqueda}
+                        value={pais}
+                    >
+                        <option value=""> Seleccione un País </option>
+                        <option value="US">Estados Unidos</option>
+                        <option value="MX">México</option>
+                        <option value="AR">Argentina</option>
+                        <option value="CO">Colombia</option>
+                        <option value="CR">Costa Rica</option>
+                        <option value="ES">España</option>
+                        <option value="PE">Perú</option>
+                    </select>
+                </div>
+
                 <input 
-                    type="text" 
-                    id="ciudad"
-                    name="ciudad"
+                    type="submit"
+                    value='Consultar Clima' 
                 />
-            </div>
-            <div className="campo">
-                <label htmlFor="ciudad">Pais</label>
-                <select 
-                    name="pais" 
-                    id="pais"
-                >
-                    <option value=""> Seleccione un País </option>
-                    <option value="US">Estados Unidos</option>
-                    <option value="MX">México</option>
-                    <option value="AR">Argentina</option>
-                    <option value="CO">Colombia</option>
-                    <option value="CR">Costa Rica</option>
-                    <option value="ES">España</option>
-                    <option value="PE">Perú</option>
-                </select>
-            </div>
 
-            <input 
-                type="submit"
-                value='Consultar Clima' 
-            />
-
-        </form>
-    </div>
-  )
+            </form>
+        </div>
+    )
 }
